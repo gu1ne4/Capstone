d1d8c6f4b76eb6cd4b9655724abc3f9de37e9df8
gu1ne4 <sungaynipower22@gmail.com>
2026-01-27 18:59:49 +0800

    Added Imports to App.js

--- capstoneFile/App.js
+++ capstoneFile/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import homePage from './mainComponents/homePage';
+import loginPage from './mainComponents/loginPage';
+import createAccount from './userComponents/createAccount';
+import editAccount from './userComponents/editAccount';
+import userDashboard from './userComponents/userDashboard';
+
 import styles from './styles/StyleSheet';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Capstone File</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
