2bcf25f40242c292e99aa82ebe9a11b2f83d8725
gu1ne4 <sungaynipower22@gmail.com>
2026-01-27 18:55:08 +0800

    File & Folder Modifications

---
+++ capstoneFile/mainComponents/homePage.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function homePage() {
+  return (
+    <View>
+      <Text>homePage</Text>
+    </View>
+  )
+}
--- capstoneFile/userComponents/createAccount.jsx
+++ capstoneFile/userComponents/createAccount.jsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function signupPage() {
+export default function createAccount() {
   return (
     <View>
-      <Text>signupPage</Text>
+      <Text>createAccount</Text>
     </View>
   )
 }
